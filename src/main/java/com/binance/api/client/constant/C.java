2e5466551a910d6c19a089dcff9e5bfa7f141662
wangwei <wei.wang@visualchina.com>
2017-11-13 17:14:24 +0800

    ignore C.java

--- src/main/java/com/binance/api/client/constant/C.java
+++ src/main/java/com/binance/api/client/constant/C.java
@@ -6,8 +6,8 @@ package com.binance.api.client.constant;
 public class C {
 
  
-  public static final String API_KEY = "tnBenv4qjBKn09z3N5Q55us2pepDmi67o1HUaRPXIkGxHE9H3sOL3";
-  public static final String SECRET = "sHZlLncuqm38OubZD20JSITPDFHQ03XLTbDlNj1ogdC7WPk3oiHoSl";
+  public static final String API_KEY = "tnBenv4qjBKn09z3N5Q55i9IUul5Gp2Xus2pepDmi67o1HUaRPXIkGxHE9H3sOL3";
+  public static final String SECRET = "sHZlLncuqm38OubZDlooDLsFW2h20JSITPDFHQ03XLTbDlNj1ogdC7WPk3oiHoSl";
   
  
 }
